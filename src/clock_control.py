8a50540c5938dd72a6029311760076f472b928f2
Robin Heitz <robin_rob96@me.com>
2021-05-18 16:47:46 +0200

    small changes

--- src/clock_control.py
+++ src/clock_control.py
@@ -8,10 +8,6 @@ from pixel_definition import (
     )
 from pixel_controller import PixelController
 
-# import pixel_definition
-# import pixel_controller
-
-
 
 
 # BIRTH_DAY = (5,17) #month, day
@@ -73,11 +69,8 @@ def activate_words(controller, min, h):
         controller.activatePixels(WD_5_MIN_BEFORE_HALF)
     elif 30<= min < 35:
         controller.activatePixels(WD_HALF)
-    
     elif 35<= min < 40:
         controller.activatePixels(WD_5_MIN_AFTER_HALF)
-    
-   
     elif 40<= min < 45:
         controller.activatePixels(WD_20_BEFORE)
     elif 45<= min < 50:
@@ -135,73 +128,40 @@ if __name__ == "__main__":
 
     controller = PixelController()
     
-    while True:
-        utc = pytz.timezone('UTC')
-        now = utc.localize(datetime.utcnow())
+    try:
+        while True:
+            utc = pytz.timezone('UTC')
+            now = utc.localize(datetime.utcnow())
 
-        local_tz = pytz.timezone('Europe/Berlin')
-        local_time = now.astimezone(local_tz)
+            local_tz = pytz.timezone('Europe/Berlin')
+            local_time = now.astimezone(local_tz)
 
-        y = local_time.year
-        m = local_time.month
-        d = local_time.day
-        hour_pre_transform = local_time.hour
-        min = local_time.minute
-        hour = translate_to_12h_clock(hour_pre_transform)
+            y = local_time.year
+            m = local_time.month
+            d = local_time.day
+            hour_pre_transform = local_time.hour
+            min = local_time.minute
+            hour = translate_to_12h_clock(hour_pre_transform)
 
-        if m == BIRTH_DAY[0] and d == BIRTH_DAY[1]:
-            print("ITS HER BIRTHDAY")
-            birthday = True
-        
-
-        #settings minute points
-        min_points = min % 5
-        activate_minute_dots(controller, min_points)
-
-        activate_it_is(controller)
-
-        activate_hour(controller,min, hour)
-
-        activate_words(controller, min, hour)
+            if m == BIRTH_DAY[0] and d == BIRTH_DAY[1]:
+                print("ITS HER BIRTHDAY")
+                birthday = True
             
 
+            #settings minute points
+            min_points = min % 5
+            activate_minute_dots(controller, min_points)
 
-        time.sleep(1)
+            activate_it_is(controller)
 
+            activate_hour(controller,min, hour)
 
-        # if min == 0:
-        #     print(f"Es ist {h} Uhr") 
-
-        # elif 0 < min < 5:
-        #     print(f"Es ist {h} Uhr +{min}.")
-
-        # elif 5 <= min < 25:
-        #     if min < 10:
-        #         print(f"Es ist 5 min nach {h} + {min-5}")
-        #     elif min < 15:
-        #         print(f"Es ist 10 min nach {h} + {min-10}")
-        #     elif min < 20:
-        #         print(f"Es ist viertel nach {h} + {min-15}")
-        #     elif min < 25:
-        #         print(f"Es ist zwanzig nach {h} + {min-20}")
+            activate_words(controller, min, hour)
+                
 
 
-
-        # elif 25 <= min <= 39:
-        #     if min < 30:
-        #         print(f"Es ist fuenf vor halb {next_hour(h)} + {min-25}")
-        #     elif min < 35:
-        #         print(f"Es ist halb {next_hour(h)} + {min-30}")
-        #     else:
-        #         print(f"Es ist fuenf nach halb {next_hour(h)} + {min-35}")
-
-        # elif 40 <= min :
-        #     if min < 45:
-        #         print(f"Es ist zwanzig vor {next_hour(h)} + {min-40}")
-        #     elif min < 50:
-        #         print(f"Es ist viertel vor {next_hour(h)} + {min-45}")
-        #     elif min < 55:
-        #         print(f"Es ist zehn vor {next_hour(h)} + {min-50}")
-        #     else:
-        #         print(f"Es ist fuenf vor {next_hour(h)} + {min-55}")
+            time.sleep(1)
+    
+    except KeyboardInterrupt:
+        controller.deactivatePixels(list(range(16**2)))
 
