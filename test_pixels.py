6c56305413fef993b55ce7291ad824e97649a7ed
Robin Heitz <blk_rh@wzl.rwth-aachen.de>
2022-06-26 18:03:42 +0200

    testing

--- test_pixels.py
+++ test_pixels.py
@@ -1,6 +1,7 @@
 from rpi_ws281x import *
 import time
-LED_COUNT      = 16**2      # Number of LED pixels.
+# LED_COUNT      = 16**2      # Number of LED pixels.
+LED_COUNT      = 50      # Number of LED pixels.
 LED_PIN        = 12      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
