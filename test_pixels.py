ff201f29a9b747df039f9923e8bd0d0371992a7b
Robin Heitz <blk_rh@wzl.rwth-aachen.de>
2022-06-27 19:15:48 +0200

    testing

--- test_pixels.py
+++ test_pixels.py
@@ -1,9 +1,7 @@
 from rpi_ws281x import *
 import time
-# LED_COUNT      = 16**2      # Number of LED pixels.
-LED_COUNT      = 50      # Number of LED pixels.
-LED_PIN        = 12      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_COUNT      = 16**2      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 50     # Set to 0 for darkest and 255 for brightest
@@ -24,6 +22,16 @@ if __name__ == "__main__":
     
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
+
+    print("Booting up...")
+
+    for i in range(16**2):
+        strip.setPixelColor(i, Color(0,0,0))
+    strip.show()
+
+    print("Waiting ...")
+
+    time.sleep(5)
 
     for i in range(16**2):
         
