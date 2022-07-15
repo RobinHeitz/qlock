db157516e9275db5272859cb6a043a73269d6e68
Robin Heitz <blk_rh@wzl.rwth-aachen.de>
2022-07-15 20:54:39 +0200

    .

--- load_config.py
+++ load_config.py
@@ -23,8 +23,8 @@ def load_config_from_file():
         birthday =  loaded_config.get('birthday', dict(month=6, day=14))
         early_morning = loaded_config.get('early_morning', dict(start_hour=6, start_min=15, end_hour=6, end_min=45))
         early_night = loaded_config.get('early_night', dict(start_hour=21, start_min=15, end_hour=21, end_min=45))
-        late_morning = loaded_config.get('early_morning', dict(start_hour=8, start_min=45, end_hour=9, end_min=15))
-        late_night = loaded_config.get('early_night', dict(start_hour=22, start_min=30, end_hour=23, end_min=0))
+        late_morning = loaded_config.get('late_morning', dict(start_hour=8, start_min=45, end_hour=9, end_min=15))
+        late_night = loaded_config.get('late_night', dict(start_hour=22, start_min=30, end_hour=23, end_min=0))
 
         
         return birthday, early_morning, early_night, late_morning, late_night
