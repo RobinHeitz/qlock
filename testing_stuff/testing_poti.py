bb37bcc16b85664a808e95bd0aab1470b065a22d
Robin Heitz <blk_rh@wzl.rwth-aachen.de>
2022-08-07 21:13:59 +0200

    brightness

--- testing_stuff/testing_poti.py
+++ testing_stuff/testing_poti.py
@@ -28,7 +28,7 @@ while True:
     value = 0
     for i in range(4):
         # Read the specified ADC channel using the previously set gain value.
-        value = adc.read_adc(0, gain=GAIN)
+        value = adc.read_adc(0, gain=GAIN, data_rate = 128)
         # values[4] = poti_brightness(values[0])
         # Note you can also pass in an optional data_rate parameter that controls
         # the ADC conversion time (in samples/second). Each chip has a different
@@ -41,4 +41,4 @@ while True:
     # print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} | {4:>6} '.format(*values))
     print("Value = ", value, "brightness factor = ", get_poti_brightness(value))
     # Pause for half a second.
-    time.sleep(0.5)
+    time.sleep(0.01)
